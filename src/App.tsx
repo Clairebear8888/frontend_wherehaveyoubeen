6086cc9ec49b1cca658432ea1eb2309d6f2d0e96
Claire Zhu <claireliwen111@gmail.com>
2025-11-12 10:51:24 +0100

    Unnest folders

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import CountryDetailsPage from "./pages/CountryDetailsPage";
+import CountryFormPage from "./pages/CountryFormPage";
+import MemoryFormPage from "./pages/MemoryFormPage";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/countries/:id" element={<CountryDetailsPage />} />
+          <Route path="/countries/new" element={<CountryFormPage />} />
+          <Route path="/countries/:id/edit" element={<CountryFormPage />} />
+          <Route
+            path="/countries/:countryId/memories/new"
+            element={<MemoryFormPage />}
+          />
+          <Route path="/memories/:id/edit" element={<MemoryFormPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
